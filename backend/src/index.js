73d993afef17abd02c80eb401eafe49f6ea059ba
SebastianOso <sebastian.oso03@gmail.com>
2025-12-09 17:57:39 -0600

    feat: connect to database first

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,10 +9,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4002;
 
-connectDB();
+
 
 app.use(express.json());
 app.use(rateLimiter)
 app.use("/api/notes", notesRouter);
 
-app.listen(PORT, () => console.log("Server running on", PORT));
+connectDB().then(() => {
+    app.listen(PORT, () => console.log("Server running on", PORT));
+});
