18c976a9e058d0c39e57160942f871a4b339d601
SebastianOso <sebastian.oso03@gmail.com>
2025-12-09 19:22:37 -0600

    feat: navbar created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,6 @@ import NoteDetailPage from './pages/NoteDetailPage'
 const App = () => {
   return (
     <div data-theme="forest">
-      <button className='btn btn-primary'>Click me</button>
       <Routes>
         <Route path='/' element={<HomePage />}/>
         <Route path='/create' element={<CreatePage />}/>
